37af254c98528ca3b4014ab720f8468a585d8e68
Jay <leafjay001@gmail.com>
2023-05-08 22:47:26 +0200

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,39 @@
-'use client'
+"use client";
 
+import clsx from "clsx";
 import Link from "next/link";
-import { usePathname } from "next/navigation"
-
+import styles from "./styles.module.css";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-
-
   const MENU_ITEM = [
-    {name : "Works", href : "/works"},
-    {name : "About", href : "/about"},
-  ]
+    { name: "Works", href: "/works" },
+    { name: "About", href: "/about" },
+  ];
 
   const pathname = usePathname();
 
-  return(
-    <nav className="flex w-full space-x-8">
+  return (
+    <nav className="flex w-full space-x-8 p-8 fixed top-0 shadow">
       <div className="flex-grow">
         <i>
           <Link href="/">Home</Link>
         </i>
       </div>
 
-      {MENU_ITEM.map((item,idx) => {
-        const isActive = pathname.startsWith(item.href)
+      {MENU_ITEM.map((item, idx) => {
+        const isActive = pathname.startsWith(item.href);
         return (
-          <NavItem name={item.name} href={item.href} key={idx} active={isActive}/>
+          <NavItem
+            name={item.name}
+            href={item.href}
+            key={idx}
+            active={isActive}
+          />
         );
       })}
     </nav>
-  )
+  );
 }
 
 function NavItem({
@@ -40,12 +44,15 @@ function NavItem({
   name: string;
   href: string;
   active: boolean;
-}) { 
-  return(
-  <div className={active ? "nav--item__active" : "nav--item"}>
-    <Link href={href}>
-      {name}
-    </Link>
-  </div>
-  )
+}) {
+  return (
+    <div
+      className={clsx({
+        [styles.navItem]: true,
+        [styles.active]: active,
+      })}
+    >
+      <Link href={href}>{name}</Link>
+    </div>
+  );
 }
