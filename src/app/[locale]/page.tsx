cad204f63eca646decab854f396c3c982b1b3f2a
Jay <leafjay001@gmail.com>
2023-05-16 10:03:59 +0200

    hero

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { Link, useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <>
       <section className="h-screen">
-        <div className="h-full flex justify-center items-center flex-col">
-          <h1 className="text-9xl text-center mb-16">Web J</h1>
+        <div className="flex w-full h-full space-x-8">
+          <div className="flex-1 flex justify-center flex-col h-full space-y-8">
+            <h1 className="font-black text-6xl">Websites that make your competition jealous</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptates autem eum iure quo beatae ipsum facilis, quisquam maxime, officia, libero mollitia deleniti pariatur nisi distinctio ratione deserunt quidem commodi.</p>
+            <a href="/works" className="border-2 rounded-lg w-fit p-4">View my works</a>
+          </div>
+          <div className="flex-1 flex flex-col justify-center h-full">
+            <div className="relative flex-grow my-40">
+              <Image src="/neco.jpg" alt="a neco arc image" fill/>
+            </div>
+          </div>
         </div>
       </section>
     </>
